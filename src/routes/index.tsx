4a8939e90ab42e7d0a9f319ced4831b18cfce1a5
limtjdghks <epaktldk0203@gmail.com>
2025-08-16 22:54:35 +0900

    feat: router 설정 수정 + toss 테스트 페이지 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,20 +3,43 @@ import { MainPage } from '@/pages/MainPage'
 import { NotFoundPage } from '@/pages/NotFound'
 import SocialLogin from '@/pages/SocialLogin'
 import LoginPage from '@/pages/LoginPage'
+import { PaymentCheckoutPage, SuccessPage, FailPage } from '@/shared/api/TossPayments'
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <MainPage />,
     errorElement: <NotFoundPage />,
-  },
-  {
-    path: '/login',
-    element: <LoginPage />,
-  },
-  {
-    path: '/social/:provider',
-    element: <SocialLogin />,
+    children: [
+      {
+        index: true,
+        element: <MainPage />,
+      },
+      {
+        path: 'login',
+        element: <LoginPage />,
+      },
+      {
+        path: 'social/:provider',
+        element: <SocialLogin />,
+      },
+      {
+        path: 'billing',
+        children: [
+          {
+            path: 'payment',
+            element: <PaymentCheckoutPage />,
+          },
+          {
+            path: 'payment/success',
+            element: <SuccessPage />,
+          },
+          {
+            path: 'payment/fail',
+            element: <FailPage />,
+          },
+        ],
+      },
+    ],
   },
 ])
 
