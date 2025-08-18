0a5bade5ab450fc672feb939672efedf58a3ecca
limtjdghks <epaktldk0203@gmail.com>
2025-08-19 01:08:05 +0900

    feat: 소셜로그인 버튼 구현

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -26,3 +26,18 @@ export const Button:React.FC<ButtonProps> = ({text, className, disabled, variant
             <button className={`py-[8.5px] px-[6.5px] min-w-16 rounded-xl font-bold ${className} ${disabled ? 'opacity-40' : 'hover:cursor-pointer'} ${variant === 'primary' ? ` text-gray-100 ${preseed ? 'bg-primary-800' : 'bg-primary'} ` : `text-primary-900 border border-primary ${preseed ? 'bg-primary' : 'bg-primary-100'}`}`} onClick={onClick}>{text}</button>
     )
 }
+
+interface SocialButtonProps {
+    text: string;
+    className?: string;
+    onClick?: () => void;
+    icon: string;
+}
+export const SocialButton:React.FC<SocialButtonProps> = ({text, className, onClick, icon}) => {
+    return (
+        <button className={`py-[17px] px-[6.5px] w-full rounded-sm font-bold ${className} hover:cursor-pointer flex items-center gap-2`} onClick={onClick}>
+            <img src={icon} alt='' />
+            <span>{text}</span>
+        </button>
+    )
+}
