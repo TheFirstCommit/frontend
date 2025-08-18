5495a53bb619279b33cbaecb05b52d50734fcecd
limtjdghks <epaktldk0203@gmail.com>
2025-08-19 01:08:05 +0900

    feat: 소셜로그인 버튼 구현

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,18 +1,43 @@
-import { useNavigate } from "react-router-dom"
+import { SocialButton } from '@/components/Buttons'
+import { useNavigate } from 'react-router-dom'
+import Icon_Kakao from '@/assets/icons/icon_Kakao.svg'
+import Icon_Google from '@/assets/icons/icon_Google.svg'
+import Icon_Naver from '@/assets/icons/icon_Naver.svg'
 
-const LoginPage:React.FC = () => {
-    const navigate = useNavigate()
-    const handleSocialLogin = async (provider:string) => {
-        navigate(`/public/social/${provider}`)
-    }
+const LoginPage: React.FC = () => {
+  const navigate = useNavigate()
+  const handleSocialLogin = async (provider: string) => {
+    navigate(`/public/social/${provider}`)
+  }
 
-    return(
-        <div className="flex mt-20 items-center gap-10">
-            <button className="hover:cursor-pointer border-2 border-gray-300 rounded-md p-2" onClick={() => handleSocialLogin('kakao')}>Kakao</button>
-            <button className="hover:cursor-pointer border-2 border-gray-300 rounded-md p-2" onClick={() => handleSocialLogin('google')}>Google</button>
-            <button className="hover:cursor-pointer border-2 border-gray-300 rounded-md p-2" onClick={() => handleSocialLogin('naver')}>Naver</button>
+  return (
+    <div className="flex items-center gap-10 bg-background">
+      <div className='flex flex-col justify-center w-full px-5'>
+
+        {/* 소셜 로그인 버튼 */}
+        <div className="flex flex-col gap-4">
+            <SocialButton
+              className="bg-[#03C75A] text-white"
+              text="네이버로 시작하기"
+              icon={Icon_Naver}
+              onClick={() => handleSocialLogin('naver')}
+            />
+          <SocialButton
+            className="bg-[#FEE500] text-gray-900"
+            text="카카오로 시작하기"
+            icon={Icon_Kakao}
+            onClick={() => handleSocialLogin('kakao')}
+          />
+          <SocialButton
+            className="bg-white drop-shadow-sm text-gray-700"
+            text="구글로 시작하기"
+            icon={Icon_Google}
+            onClick={() => handleSocialLogin('google')}
+          />
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default LoginPage
