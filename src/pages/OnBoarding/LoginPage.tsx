9a25a609dd1327ef49543f73e172befb7dd58cd9
limtjdghks <epaktldk0203@gmail.com>
2025-08-21 15:57:11 +0900

    fix: 로그인 요청 URL 수정

--- src/pages/OnBoarding/LoginPage.tsx
+++ src/pages/OnBoarding/LoginPage.tsx
@@ -1,26 +1,22 @@
 import { SocialButton } from '@/components/Buttons'
-import { useNavigate } from 'react-router-dom'
 import Icon_Kakao from '@/assets/icons/icon_Kakao.svg'
 import Icon_Google from '@/assets/icons/icon_Google.svg'
 import Icon_Naver from '@/assets/icons/icon_Naver.svg'
 
 const LoginPage: React.FC = () => {
-  const navigate = useNavigate()
   const handleSocialLogin = async (provider: string) => {
-    navigate(`/public/social/${provider}`)
+    window.location.href = `http://localhost:8080/public/social/${provider}`
   }
 
   return (
     <div className="flex gap-10 bg-background min-h-dvh">
       <div className="flex flex-col justify-center w-full px-5">
-
         <div className="flex justify-center mt-auto mb-auto">
           <div className="w-[180px] h-[90px] bg-gray-200">로고자리</div>
         </div>
 
         {/* 소셜 로그인 버튼 */}
         <div className="flex flex-col gap-4 mt-auto mb-20">
-
           <div className="flex items-center justify-center mb-2">
             <div className="flex-1 h-0.5 bg-gray-400"></div>
             <span className="mx-4 text-sm font-semibold">소셜 계정 간편 로그인 / 회원가입</span>
