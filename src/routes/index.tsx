710c6b9aaba08a043bbb19cf7010c7759e15a552
Seongmin0218 <yyggy365@gmail.com>
2025-08-22 10:23:19 +0900

    소식함 UX/UI완료

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -27,6 +27,7 @@ import MyInfo_EditPage from '@/pages/MyPage/MyInfo_EditPage'
 import Group_Payment from '@/pages/MyPage/Group_Payment'
 import LeavePage from '@/pages/MyPage/LeavePage'
 import HomePage from '@/pages/HomePage'
+import FeedboxPage from '@/pages/FeedboxPage'
 
 export const router = createBrowserRouter([
   {
@@ -135,6 +136,10 @@ export const router = createBrowserRouter([
         path: 'home',
         element: <HomePage />
       },
+      {
+        path: '/feedbox',
+        element: <FeedboxPage />
+      }
     ],
   },
   {
